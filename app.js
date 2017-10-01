51b997493e1d8f8e8e6cb2826c911eaadc71a693
Your Name <you@example.com>
2017-10-01 18:17:10 +0200

    add styles

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
-var app = require('express')();
+#!/usr/local/n/versions/node/7.9.0/bin/node
+
+var express = require('express');
+var app = express();
 var http = require('http');
 var server = http.Server(app);
 var swig = require('swig');
 var path = require('path');
 
-var hue = "0.0.0.0";
-var key = "getakey";
+var hue = "192.168.0.133";
+var key = "PkONUdT2XFC87UmSgBanD1UD-ET-Rfghl8RnQUK2";
+
 
 // view engine setup
 app.engine('html', swig.renderFile);
+app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
+app.use(express.static('public'))
 
 // server and routing
 server.listen(8080);
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.render('index');
 });
 
 
 
+
 var io = require('socket.io')(server);
 // socket.io demo
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
 
-  http.get('http://'+hue+'/api/'+key+'/lights', (res) => {
-    const { statusCode } = res;
+  http.get('http://' + hue + '/api/' + key + '/lights', (res) => {
+    const {
+      statusCode
+    } = res;
     const contentType = res.headers['content-type'];
 
     let error;
     if (statusCode !== 200) {
       error = new Error(`Request Failed.\n` +
-                        `Status Code: ${statusCode}`);
+        `Status Code: ${statusCode}`);
     } else if (!/^application\/json/.test(contentType)) {
       error = new Error(`Invalid content-type.\n` +
-                        `Expected application/json but received ${contentType}`);
+        `Expected application/json but received ${contentType}`);
     }
     if (error) {
       console.error(error.message);
@@ -44,11 +53,13 @@ io.on('connection', function (socket) {
 
     res.setEncoding('utf8');
     let rawData = '';
-    res.on('data', (chunk) => { rawData += chunk; });
+    res.on('data', (chunk) => {
+      rawData += chunk;
+    });
     res.on('end', () => {
       try {
         const parsedData = JSON.parse(rawData);
-        // console.log(parsedData);
+        console.log('Setting up a new client');
         socket.emit('server event', parsedData)
       } catch (e) {
         console.error(e.message);
@@ -59,15 +70,21 @@ io.on('connection', function (socket) {
   });
 
 
-  socket.on('client event', function (data) {
+  socket.on('client event', function(data) {
+    console.log(new Date(), 'Recieved data from client')
     var options = {
       host: hue,
       port: 80,
-      path: '/api/'+key+'/lights/2/state',
-      method: 'PUT'
+      path: '/api/' + key + '/lights/2/state',
+      method: 'PUT',
+      headers: {
+        "Content-Type" : "application/json",
+      }
     };
     http.request(options, function(response) {
       console.log(data);
-    }).write(JSON.stringify(data))
+      io.emit('server update', data);
+      console.log(new Date(), 'Updating clients')
+    }).end(JSON.stringify(data));
   });
 });
